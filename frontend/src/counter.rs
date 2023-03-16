771aca007fe140e7f7e7e9235a4e4ca0451f08f5
Andre Francois <andre.francois@gmail.com>
2023-03-16 01:36:37 -0400

    Compiles but does not work.

--- frontend/src/counter.rs
+++ frontend/src/counter.rs
@@ -2,15 +2,58 @@ use gloo_net::http::Request;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
+fn call_api(path: String, data: UseStateHandle<Option<u32>>) {
+    spawn_local(async move {
+        let resp = Request::post(format!("/api/{}/", path).as_str()).send().await.unwrap();
+        if !resp.ok() {
+            tracing::error!(
+                "Error fetching data {} ({})",
+                resp.status(),
+                resp.status_text()
+            );
+            return;
+        }
+
+        let content = match resp.text().await {
+            Err(err) => {
+                tracing::error!("Error fetching data {err}");
+                return;
+            }
+            Ok(content) => content,
+        };
+
+        let count = match content.parse() {
+            Err(err) => {
+                tracing::error!("Data is not a number: {err}");
+                return;
+            }
+            Ok(count) => count,
+        };
+
+        data.set(Some(count))
+    });
+}
+
 #[function_component(Counter)]
 pub fn counter() -> Html {
     let data = use_state(|| Option::<u32>::None);
+    let inc_data = data.clone();
+    let oninc = Callback::from(move |_| {
+        let inc_data = inc_data.clone();
+        call_api("increment".to_string(), inc_data);
+    });
+    let dec_data = data.clone();
+    let ondec = Callback::from(move |_| {
+        let dec_data = dec_data.clone();
+        call_api("decrement".to_string(), dec_data);
+    });
+
     {
         let data = data.clone();
         use_effect(move || {
             if data.is_none() {
                 spawn_local(async move {
-                    let resp = Request::post("/api/counter").send().await.unwrap();
+                    let resp = Request::post("/api/counter/").send().await.unwrap();
 
                     if !resp.ok() {
                         tracing::error!(
@@ -49,13 +92,13 @@ pub fn counter() -> Html {
         <div class="container">
             <div class="row">
                 <div class="col">
-                    { "Count: " }
+                    { "Counter: " }
                     {data.map(|d|d.to_string()).unwrap_or_default()}
                 </div>
                 <div class="col">
-                    <button type="button" class="btn btn-danger">{ "v" }</button>
+                    <button onclick={oninc} type="button" class="btn btn-danger">{ "v" }</button>
                     { " " }
-                    <button type="button" class="btn btn-success">{ "^" }</button>
+                    <button onclick={ondec} type="button" class="btn btn-success">{ "^" }</button>
                 </div>
                 <div class="col">{ " " }</div>
             </div>
--- server/src/counter.rs
+++ server/src/counter.rs
@@ -8,16 +8,29 @@ struct Counter {
 }
 
 pub(crate) fn setup(router: Router) -> Router {
+    let api_routes = Router::new()
+        .nest("/counter", Router::new().route("/", post(counter)))
+        .nest("/increment", Router::new().route("/", post(increment)))
+        .nest("/decrement", Router::new().route("/", post(decrement)));
     router
-        .nest(
-            "/api",
-            Router::new().route("/counter", post(server_counter)),
-        )
+        .nest("/api", api_routes)
         .layer(Extension(Arc::new(Mutex::new(Counter::default()))))
 }
 
-async fn server_counter(Extension(counter): Extension<Arc<Mutex<Counter>>>) -> impl IntoResponse {
+async fn increment(Extension(counter): Extension<Arc<Mutex<Counter>>>) -> impl IntoResponse {
     let mut counter = counter.lock().unwrap();
     counter.count += 1;
+    format!("{}", counter.count)
+}
+
+async fn decrement(Extension(counter): Extension<Arc<Mutex<Counter>>>) -> impl IntoResponse {
+    let mut counter = counter.lock().unwrap();
+    counter.count -= 1;
+    format!("{}", counter.count)
+}
+
+
+async fn counter(Extension(counter): Extension<Arc<Mutex<Counter>>>) -> impl IntoResponse {
+    let counter = counter.lock().unwrap();
     format!("{}", counter.count)
 }
